07e5a6eeb3ee8ff734a369c94975c92f1710c8d0
Sandeeep Kumar Yadav <sandeepdeoria555@gmail.com>
2025-09-16 00:44:56 +0530

    Implement code changes to enhance functionality and improve performance

--- app/page.js
+++ app/page.js
@@ -1,4 +1,9 @@
+import HeroSection from "@/components/hero";
+
 export default function Home() {
-  return <div className="mt-40">
-  </div>;
+  return ( 
+    <div className="mt-40">
+      <HeroSection />                {/*server component  for static page*/}
+    </div>
+  );
 }
---
+++ components/hero.jsx
@@ -0,0 +1,48 @@
+"use client";                                         {/*client component for dymanic*/ }    
+
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
+const HeroSection = () => {
+  return ( 
+    <div className="pb-20 px-4">
+        <div className="container mx-auto text-center">
+            <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
+            Manage Your Finances <br /> with Intelligence
+            </h1>
+            <p>
+            An AI-powered financial management platform that helps you track,
+            analyze, and optimize your spending with real-time insights.
+            </p>
+            <div>
+            <Link href="/dashboard">
+                <Button size="lg" className="px-8">
+                Get Started
+                </Button>
+            </Link>
+            <Link href="https://github.com/SandeepKumarYadav24">
+                <Button size="lg" variant="outline" className="px-8">
+                Watch Github
+                </Button>
+            </Link>
+            </div>
+            <div>
+                <div>
+                <Image
+                    src="/banner.jpeg"
+                    width={1280}
+                    height={720}
+                    alt="Dashboard Preview"
+                    className="rounded-lg shadow-2xl border mx-auto"
+                    priority
+                />
+                </div>
+            </div>
+        </div>
+    </div>
+  
+  );
+};
+
+export default HeroSection;
