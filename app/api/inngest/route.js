0345775624f15db3dd618cb00a731692d94815af
Sandeeep Kumar Yadav <sandeepdeoria555@gmail.com>
2025-09-25 02:42:08 +0530

    Add checkBudgetAlerts function and update route to serve it

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -4,10 +4,9 @@ export const maxDuration = 300;
 import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { helloWorld } from "@/lib/inngest/functions";
+import { checkBudgetAlerts } from "@/lib/inngest/functions";
 
-export const { GET, POST, PUT } = serve({
+export const { GET, POST, PUT, OPTIONS } = serve({
   client: inngest,
-  functions: [
-    helloWorld, // all your functions get added here
-  ],
+  functions: [checkBudgetAlerts],
 });
