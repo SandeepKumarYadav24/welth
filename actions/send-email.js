0aa0bbaaf8ea735e1570c15e0f390e4f57375938
Sandeeep Kumar Yadav <sandeepdeoria555@gmail.com>
2025-09-27 09:49:20 +0530

    Refactor sendEmail function and improve error handling; clean up checkBudgetAlerts function formatting

--- actions/send-email.js
+++ actions/send-email.js
@@ -1,19 +1,21 @@
 "use server";
 
 import { Resend } from "resend";
-import { render } from '@react-email/render';
 
-export async function sendEmail({ to, subject, react }) {
+
+export async function sendEmail({ to, subject, react}) {
   const resend = new Resend(process.env.RESEND_API_KEY || "");
 
   try {
-    const data = await resend.emails.send({
-      from: "Finance App <onboarding@resend.dev>",
+    const { data , error} = await resend.emails.send({
+      from: "Finance App <noreply@heysandeep.dev>",
       to,
       subject,
       react,
     });
-
+    if(error){
+      console.log(error.message);
+    }
     return { success: true, data };
   } catch (error) {
     console.error("Failed to send email:", error);
