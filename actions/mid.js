ef7c2d9f9b41fa7b12c9993ade119085e857c05a
Sandeeep Kumar Yadav <sandeepdeoria555@gmail.com>
2025-09-29 20:50:59 +0530

    Add ArcJet middleware for request protection and bot detection; update package-lock for installation script

---
+++ actions/mid.js
@@ -0,0 +1,25 @@
+import arcjet from "@arcjet/next";
+
+// Create Arcjet middleware
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  // characteristics: ["userId"], // Track based on Clerk userId
+  rules: [
+    // Shield protection for content and security
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+        "GO_HTTP", // For Inngest
+        // See the full list at https://arcjet.com/bot-list
+      ],
+    }),
+  ],
+});
+
+export {
+    aj
+};
